ef8a87b62a12f3ce609c5ca81f0192b9c737d004
Alex Jang <aljang@deloitte.nl>
2024-06-28 09:10:18 +0200

    the main file

---
+++ main.py
@@ -0,0 +1,33 @@
+import argparse
+
+from src.utils import load_env_file
+
+from src.preprocess import run_preprocessing
+from src.mapping import run_ledger_mapping
+from src.evaluate import run_manual_inspection
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", type=str, default="./data/input/companyinfo")
+    parser.add_argument("--data_dir", type=str, default="./data/dataset")
+    parser.add_argument("--save_dir", type=str, default="./data/output")
+    parser.add_argument("--res_dir", type=str, default="./data/resources")
+
+    parser.add_argument("--provider", type=str, default="hf", choices=["openai", "hf"])
+
+    load_env_file()
+
+    args = parser.parse_args()
+
+    run_preprocessing(input_dir=args.input_dir, save_dir=args.data_dir)
+
+    run_ledger_mapping(
+        args.provider,
+        args.data_dir,
+        args.res_dir,
+        args.save_dir,
+    )
+
+    run_manual_inspection(args.data_dir, args.res_dir, args.save_dir)
