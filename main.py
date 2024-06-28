0c6412c350f38e5ed750280ecb1c834b22bd8dd0
Alex Jang <aljang@deloitte.nl>
2024-06-28 09:23:58 +0200

    add doc_store dir

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir", type=str, default="./data/dataset")
     parser.add_argument("--save_dir", type=str, default="./data/output")
     parser.add_argument("--res_dir", type=str, default="./data/resources")
+    parser.add_argument("--doc_store_dir", type=str, default="./data/doc_store")
 
     parser.add_argument("--provider", type=str, default="hf", choices=["openai", "hf"])
 
@@ -27,6 +28,7 @@ if __name__ == "__main__":
         args.provider,
         args.data_dir,
         args.res_dir,
+        args.doc_store_dir,
         args.save_dir,
     )
 
