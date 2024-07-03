96e2e33a88ea674133b5a79cd41168146831a00c
Alex Jang <aljang@deloitte.nl>
2024-07-03 14:27:22 +0200

    retrievers

--- src/retrievers/cpc_retriever.py
+++ src/retrievers/cpc_retriever.py
@@ -1,162 +1,181 @@
 from haystack_integrations.document_stores.chroma import ChromaDocumentStore
-
-from haystack.components.embedders import HuggingFaceAPIDocumentEmbedder
-from haystack.components.embedders import OpenAIDocumentEmbedder
-from haystack.utils import Secret
 from haystack import Document
+
+from ..utils import MyChromaDocumentStore, write_json
 
 from typing import List, Any, Dict
 
+from tqdm import tqdm
 import pandas as pd
 import os
-import time
 
 
-class TiltCPCRetriever:
-    def __init__(
-        self, provider: str, res_dir: str, doc_store_dir: str, top_k: int
-    ) -> None:
+def get_document_store(
+    provider: str, res_dir: str, doc_store_dir: str, env_key: str, model_name: str
+) -> ChromaDocumentStore:
+    """Create a new or load existing document store with CPC code and descriptions
 
-        self.provider = provider
-        self.top_k = top_k
+    Args:
+        res_dir (str): Directory to find the CPC table
+        doc_store_dir (str): Directory where to save/load doc store
 
-        # Initialise embedders to embed incoming queries for retrieval
-        if provider == "openai":
-            self.model_name = "text-embedding-3-small"
-            self.env_key = "OPENAI_API_KEY"
+    Returns:
+        ChromaDocumentStore: DocumentStore of CPC code and descriptions
+    """
 
-            self.embedder = OpenAIDocumentEmbedder(
-                api_key=Secret.from_env_var(self.env_key),
-                model=self.model_name,
-            )
-        else:
-            self.model_name = (
-                "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-            )
-            self.env_key = "HUGGINGFACEHUB_API_TOKEN"
+    doc_store_dir = f"{doc_store_dir}/cpc/{provider}"
+    doc_store_path = f"{doc_store_dir}/chroma.sqlite3"
 
-            self.embedder = HuggingFaceAPIDocumentEmbedder(
-                api_type="serverless_inference_api",
-                api_params={"model": self.model_name},
-                token=Secret.from_env_var(self.env_key),
-            )
+    # If doc_store file exists, then no need to write the documents
+    if os.path.isfile(doc_store_path):
+        print(">Load existing CPC doc store")
+        documents = None
 
-        # Initialise / Load document store with CPC descriptions for retrieval
-        self.doc_store = self.__get_doc_store(res_dir, doc_store_dir)
-
-    def __get_doc_store(self, res_dir: str, doc_store_dir: str) -> ChromaDocumentStore:
-        """Create a new or load existing document store with CPC code and descriptions
-
-        Args:
-            res_dir (str): Directory to find the CPC table
-            doc_store_dir (str): Directory where to save/load doc store
-
-        Returns:
-            ChromaDocumentStore: DocumentStore of CPC code and descriptions
-        """
-
-        doc_store_dir = f"{doc_store_dir}/cpc/{self.provider}"
-        doc_store = f"{doc_store_dir}/chroma.sqlite3"
-
-        # If doc_store file exists, then no need to write the documents
-        if os.path.isfile(doc_store):
-            print(">Load existing CPC doc store")
-            documents = None
-
-        # Else, create documents out of each CPC code and description
-        else:
-            print("> Create CPC doc store")
-            cpc_list = pd.read_csv(
-                f"{res_dir}/cpc_list.csv", dtype={"cpc": "str", "isic": "str"}
-            )
-
-            documents = [
-                Document(
-                    content=row["description"],
-                    id=row["cpc"],
-                    meta={"isic": row["isic"]},
-                )
-                for i, row in cpc_list.iterrows()
-            ]
-
-        # Set how to embed the documents depending on provider
-        if self.provider == "openai":
-            self.embedding_function = "OpenAIEmbeddingFunction"
-        else:
-            self.embedding_function = "HuggingFaceEmbeddingFunction"
-
-        # Initialise ChromaDocumentStore
-        doc_store = ChromaDocumentStore(
-            persist_path=doc_store_dir,
-            embedding_function=self.embedding_function,
-            api_key=os.environ[self.env_key],
-            model_name=self.model_name,
+    # Else, create documents out of each CPC code and description
+    else:
+        print("> Create CPC doc store")
+        cpc_list = pd.read_csv(
+            f"{res_dir}/CPC_ver_2_1.csv",
+            dtype={"Code": "str"},
         )
 
-        # Write documents if creating for the first time
-        if documents:
-            doc_store.write_documents(documents)
-
-        return doc_store
-
-    def __get_query_filters(self, isic_codes: List[str]) -> Dict[str, List[str]]:
-        """Get query filters based on the ISIC codes in Haystack filter syntax,
-        which means that metadata 'isic' has to be in the given list of ISIC codes
-
-        Args:
-            isic_codes (List[str]): List of ISIC codes
-
-        Returns:
-            Dict[str, Dict[str, List[str]]]: _description_
-        """
-
-        return {"isic": isic_codes + ["na"]}
-
-    def retrieve(
-        self, embedded_docs: List[Dict[str, Any]], isic_results
-    ) -> Dict[str, List[Document]]:
-        """For each company, retrieve the top_k results for similar CPC codes
-
-        Args:
-            data (List[Dict[str, Any]]): List of company information
-
-        Returns:
-            List[Dict[str, List[Document]]]: List of companies with top_k CPC codes
-        """
-
-        results = {}
-        for doc in embedded_docs:
-            company_id = doc.meta["company_id"]
-
-            isic_codes = isic_results[company_id]
-
-            query_filter = self.__get_query_filters(isic_codes)
-
-            # Get top_k results with the filter
-            top_results = self.doc_store.search_embeddings(
-                doc.embedding, top_k=self.top_k, filters=query_filter
+        documents = [
+            Document(
+                content=row["Description"],
+                id=row["Code"],
+                meta={"code": row["Code"]},
             )
+            for i, row in cpc_list.iterrows()
+        ]
 
-            # Structure the retrieval results neatly
-            results[company_id] = [
-                [result.id, result.meta["isic"]] for result in top_results[0]
-            ]
+    # Set how to embed the documents depending on provider
+    if provider == "openai":
+        embedding_function = "OpenAIEmbeddingFunction"
+    else:
+        embedding_function = "HuggingFaceEmbeddingFunction"
 
-            # Sleep to avoid reaching rate limit
-            time.sleep(1)
+    # Initialise ChromaDocumentStore
+    doc_store = MyChromaDocumentStore(
+        persist_path=doc_store_dir,
+        embedding_function=embedding_function,
+        api_key=os.environ[env_key],
+        model_name=model_name,
+    )
 
-        return results
+    # Write documents if creating for the first time
+    if documents:
+        doc_store.write_documents(documents)
 
-    def pretty_output(self, results: List[List[Document]]) -> Dict[str, List[List]]:
+    return doc_store
 
-        # Get the preds and scores into a dictionary
-        output = {
+
+def get_query_filters(
+    ledger: pd.DataFrame, isic_codes: List[str]
+) -> Dict[str, List[str]]:
+    """Get query filters based on the ISIC codes in Haystack filter syntax,
+    which means that metadata 'isic' has to be in the given list of ISIC codes
+
+    Args:
+        isic_codes (List[str]): List of ISIC codes
+
+    Returns:
+        Dict[str, Dict[str, List[str]]]: _description_
+    """
+
+    cpc_codes = ledger[ledger.isic_code.isin(isic_codes)]["cpc_code"].tolist()
+
+    if len(cpc_codes) == 0:
+        return {}
+
+    return {"code": cpc_codes}
+
+
+def get_ledger_isic(ledger: pd.DataFrame, cpc_code: str) -> List[str]:
+    isic_codes = ledger[ledger.cpc_code == cpc_code]["isic_code"].tolist()
+
+    return isic_codes
+
+
+def get_ledger(res_dir: str):
+    ledger = pd.read_csv(
+        f"{res_dir}/20240626_LedgerV32_PositiveOnly.txt",
+        delimiter="|",
+        dtype={"cpc_code": str, "isic_code": str},
+    )
+
+    return ledger
+
+
+def initialse(provider: str, res_dir: str, doc_store_dir: str):
+    provider = provider
+    # Initialise embedders to embed incoming queries for retrieval
+    if provider == "openai":
+        model_name = "text-embedding-3-small"
+        env_key = "OPENAI_API_KEY"
+
+    else:
+        model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+        env_key = "HUGGINGFACEHUB_API_TOKEN"
+
+    # Initialise / Load document store with CPC descriptions for retrieval
+    doc_store = get_document_store(
+        provider, res_dir, doc_store_dir, env_key, model_name
+    )
+
+    ledger = get_ledger(res_dir)
+
+    return doc_store, ledger
+
+
+def save_results(results, counter):
+    write_json(f"cpc_partial_results_{counter}.json", results)
+
+
+def retrieve(
+    embedded_docs: List[Dict[str, Any]], isic_results, doc_store, ledger, top_k
+) -> Dict[str, Dict[str, List]]:
+    """For each company, retrieve the top_k results for similar CPC codes
+
+    Args:
+        data (List[Dict[str, Any]]): List of company information
+
+    Returns:
+        List[Dict[str, List[Document]]]: List of companies with top_k CPC codes
+    """
+
+    results = {}
+
+    for doc in tqdm(embedded_docs):
+        company_id = doc.meta["company_id"]
+
+        isic_codes = isic_results[company_id]["preds"]
+
+        query_filter = get_query_filters(ledger, isic_codes)
+
+        if query_filter == {}:
+            # Get top_k results with the filter
+            top_results = doc_store.search_embeddings(doc.embedding, top_k=top_k)[0]
+        else:
+            # Get top_k results with the filter
+            top_results = doc_store.search_embeddings(
+                doc.embedding, top_k=top_k, filters=query_filter
+            )[0]
+
+        # Structure the retrieval results neatly
+        results[company_id] = {
             "preds": [
-                [cpc.id for cpc in results[company_id]] for company_id in results
+                [result.id, get_ledger_isic(ledger, result.id)]
+                for result in top_results
             ],
-            "scores": [
-                [cpc.score for cpc in results[company_id]] for company_id in results
-            ],
+            "scores": [result.score for result in top_results],
         }
 
-        return output
+    return results
+
+
+def run(embedded_docs, isic_results, provider, res_dir, doc_store_dir, top_k):
+    doc_store, ledger = initialse(provider, res_dir, doc_store_dir)
+
+    cpc_results = retrieve(embedded_docs, isic_results, doc_store, ledger, top_k)
+
+    return cpc_results
--- src/retrievers/isic_retriever.py
+++ src/retrievers/isic_retriever.py
@@ -1,247 +1,204 @@
 from haystack_integrations.document_stores.chroma import ChromaDocumentStore
-from haystack.components.embedders import HuggingFaceAPIDocumentEmbedder
-from haystack.components.embedders import OpenAIDocumentEmbedder
-from haystack.utils import Secret
+
 from haystack import Document
 
 from typing import List, Any, Dict
 
+from ..utils import MyChromaDocumentStore, read_json, write_json
+
 import pandas as pd
 import os
 import time
+from tqdm import tqdm
 
 
+def initialise(provider: str, res_dir: str, doc_store_dir: str) -> None:
 
-class TiltISICRetriever:
-    def __init__(
-        self, provider: str, res_dir: str, doc_store_dir: str, top_k: int
-    ) -> None:
+    # Initialise embedders to embed incoming queries for retrieval
+    if provider == "openai":
+        model_name = "text-embedding-3-small"
+        env_key = "OPENAI_API_KEY"
 
-        self.provider = provider
-        self.top_k = top_k
+    else:
+        model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+        env_key = "HUGGINGFACEHUB_API_TOKEN"
 
-        # Initialise embedders to embed incoming queries for retrieval
-        if provider == "openai":
-            self.model_name = "text-embedding-3-small"
-            self.env_key = "OPENAI_API_KEY"
+    # Initialise / Load document store with ISIC descriptions for retrieval
+    doc_store = get_doc_store(provider, res_dir, doc_store_dir, env_key, model_name)
 
-            self.embedder = OpenAIDocumentEmbedder(
-                api_key=Secret.from_env_var(self.env_key),
-                model=self.model_name,
+    return doc_store
+
+
+def get_doc_store(
+    provider: str, res_dir: str, doc_store_dir: str, env_key, model_name
+) -> ChromaDocumentStore:
+    """_summary_
+
+    Args:
+        res_dir (str): Directory to find the ISIC table
+        doc_store_dir (str): Directory where to save/load doc store
+
+    Returns:
+        ChromaDocumentStore: DocumentStore of ISIC code and descriptions
+    """
+
+    doc_store_dir = f"{doc_store_dir}/isic/{provider}"
+    doc_store_path = f"{doc_store_dir}/chroma.sqlite3"
+
+    # If doc_store file exists, then no need to write the documents
+    if os.path.isfile(doc_store_path):
+        print(">Load existing ISIC doc store")
+        documents = None
+
+    # Else, create documents out of each ISIC code and description
+    else:
+        print("> Create ISIC doc store")
+        isic_list = pd.read_csv(f"{res_dir}/ISIC_rev_4.csv", dtype={"Code": "str"})
+
+        documents = [
+            Document(
+                content=row["Description"],
+                id=row["Code"],
+                meta={"isic": row["Code"][:2]},
             )
+            for i, row in isic_list.iterrows()
+        ]
+
+    # Set how to embed the documents depending on provider
+    if provider == "openai":
+        embedding_function = "OpenAIEmbeddingFunction"
+    else:
+        embedding_function = "HuggingFaceEmbeddingFunction"
+
+    # Initialise ChromaDocumentStore
+    doc_store = MyChromaDocumentStore(
+        persist_path=doc_store_dir,
+        embedding_function=embedding_function,
+        api_key=os.environ[env_key],
+        model_name=model_name,
+    )
+
+    # Write documents if creating for the first time
+    if documents:
+        doc_store.write_documents(documents)
+
+    return doc_store
+
+
+def get_query_filters(isic_section: str) -> Dict[str, str]:
+    """Get query filters based on the ISIC section groups in Haystack filter syntax,
+    which means that metadata 'isic' has to equal the ISIC section
+
+    Args:
+        isic_section (str): ISIC section
+
+    Returns:
+        Dict[str, str]: ISIC filter statement according to haystack syntax
+    """
+    # TODO: needs to change to accept multiple ISIC sections for multiple SBIs
+    return {"isic": isic_section}
+
+
+def group_by_isic_section(
+    query_documents: List[Document],
+) -> Dict[str, List[Document]]:
+    """Transform company information into query documents for retrieval and
+    group by the ISIC sections based on the first two digits of the SBI code
+    """
+
+    # TODO: needs to change to accept multiple ISIC sections for multiple SBIs
+    section_groups = {}
+
+    for doc in query_documents:
+
+        query_section = doc.meta["isic_section"]
+
+        if query_section in section_groups:
+            section_groups[query_section].append(doc)
         else:
-            self.model_name = (
-                "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-            )
-            self.env_key = "HUGGINGFACEHUB_API_TOKEN"
+            section_groups[query_section] = [doc]
 
-            self.embedder = HuggingFaceAPIDocumentEmbedder(
-                api_type="serverless_inference_api",
-                api_params={"model": self.model_name},
-                token=Secret.from_env_var(self.env_key),
-            )
+    return section_groups
 
-        # Initialise / Load document store with ISIC descriptions for retrieval
-        self.doc_store = self.__get_doc_store(res_dir, doc_store_dir)
 
-    def __get_doc_store(self, res_dir: str, doc_store_dir: str) -> ChromaDocumentStore:
-        """_summary_
+def structure_results(
+    query_docs: List[Document], retrieval_results: List[List[Document]]
+) -> Dict[str, List[Document]]:
+    """Structure the retrieval results into a dictionary of company_id and
+    retrieved ISIC codes
 
-        Args:
-            res_dir (str): Directory to find the ISIC table
-            doc_store_dir (str): Directory where to save/load doc store
+    Args:
+        query_docs (List[Document]): List of query documents
+        retrieval_results (List[Document]): List of top_k ISIC codes retrieved for each query document
 
-        Returns:
-            ChromaDocumentStore: DocumentStore of ISIC code and descriptions
-        """
+    Returns:
+        Dict[str, List[Document]]: Dictionary of company_id and its top_k retrieved ISIC codes
+    """
 
-        doc_store_dir = f"{doc_store_dir}/isic/{self.provider}"
-        doc_store = f"{doc_store_dir}/chroma.sqlite3"
+    n_docs = len(query_docs)
 
-        # If doc_store file exists, then no need to write the documents
-        if os.path.isfile(doc_store):
-            print(">Load existing ISIC doc store")
-            documents = None
-
-        # Else, create documents out of each ISIC code and description
-        else:
-            print("> Create ISIC doc store")
-            isic_list = pd.read_csv(
-                f"{res_dir}/ISIC_Rev_4_english_structure.Txt", dtype={"Code": "str"}
-            )
-
-            documents = [
-                Document(
-                    content=row["Description"],
-                    id=row["Code"],
-                    meta={"isic": row["Code"][:2]},
-                )
-                for i, row in isic_list.iterrows()
-                if len(row["Code"]) >= 2
-            ]
-
-        # Set how to embed the documents depending on provider
-        if self.provider == "openai":
-            self.embedding_function = "OpenAIEmbeddingFunction"
-        else:
-            self.embedding_function = "HuggingFaceEmbeddingFunction"
-
-        # Initialise ChromaDocumentStore
-        doc_store = ChromaDocumentStore(
-            persist_path=doc_store_dir,
-            embedding_function=self.embedding_function,
-            api_key=os.environ[self.env_key],
-            model_name=self.model_name,
-        )
-
-        # Write documents if creating for the first time
-        if documents:
-            doc_store.write_documents(documents)
-
-        return doc_store
-
-    def __get_query_filters(self, isic_section: str) -> Dict[str, str]:
-        """Get query filters based on the ISIC section groups in Haystack filter syntax,
-        which means that metadata 'isic' has to equal the ISIC section
-
-        Args:
-            isic_section (str): ISIC section
-
-        Returns:
-            Dict[str, str]: ISIC filter statement according to haystack syntax
-        """
-        # TODO: needs to change to accept multiple ISIC sections for multiple SBIs
-        return {"isic": isic_section}
-
-    def __group_by_isic_section(
-        self,
-        companies: pd.DataFrame,
-        sbi_activities: pd.DataFrame,
-        companies_sbi_activities: pd.DataFrame,
-    ) -> Dict[str, List[Any]]:
-        """Transform company information into query documents for retrieval and
-        group by the ISIC sections based on the first two digits of the SBI code
-
-        Args:
-            companies (pd.DataFrame): Companies table
-            sbi_activities (pd.DataFrame): SBI activities table
-            companies_sbi_activities (pd.DataFrame): Companies - SBI activities mapper tablelist_company_info (List[Dict[str, Any]]): List of company info dictionaries
-
-        Returns:
-            Dict[str, List[Any]]: Dictionary of ISIC section codes and list of company query documents
-        """
-
-        # TODO: needs to change to accept multiple ISIC sections for multiple SBIs
-        section_groups = {}
-
-        companies = companies[["company_id", "company_description"]]
-        companies = companies.merge(companies_sbi_activities, on="company_id")
-        companies = companies.merge(sbi_activities, on="sbi_code")
-        companies["isic_section"] = companies.sbi_code.apply(lambda x: x[:2])
-        companies["query"] = companies.apply(lambda x: f"{x["sbi_code_description"]}; {x["company_description"] or ""}", axis=1)
-
-        for i, company in companies.iterrows():
-
-            # Make query documents out of company information
-            query_document = Document(
-                content=company["query"],
-                meta={"company_id": company["company_id"]},
-            )
-
-            query_section = company["isic_section"]
-
-            if query_section in section_groups:
-                section_groups[query_section].append(query_document)
-            else:
-                section_groups[query_section] = [query_document]
-
-        return section_groups
-
-    def __structure_results(
-        self, query_docs: List[Document], retrieval_results: List[List[Document]]
-    ) -> Dict[str, List[Document]]:
-        """Structure the retrieval results into a dictionary of company_id and
-        retrieved ISIC codes
-
-        Args:
-            query_docs (List[Document]): List of query documents
-            retrieval_results (List[Document]): List of top_k ISIC codes retrieved for each query document
-
-        Returns:
-            Dict[str, List[Document]]: Dictionary of company_id and its top_k retrieved ISIC codes
-        """
-
-        n_docs = len(query_docs)
-
-        results = {
-            query_docs[i].meta["company_id"]: [
-                result.id for result in retrieval_results[i]
-            ]
-            for i in range(n_docs)
+    results = {
+        query_docs[i].meta["company_id"]: {
+            "preds": [result.id for result in retrieval_results[i]],
+            "scores": [result.score for result in retrieval_results[i]],
         }
+        for i in range(n_docs)
+    }
 
-        return results
+    return results
 
-    def retrieve(
-        self,
-        companies: pd.DataFrame,
-        sbi_activities: pd.DataFrame,
-        companies_sbi_activities: pd.DataFrame,
-    ) -> Dict[str, List[Document]]:
-        """For each company, retrieve the top_k results for similar ISIC codes
 
-        Args:
-            companies (pd.DataFrame): Companies table
-            sbi_activities (pd.DataFrame): SBI activities table
-            companies_sbi_activities (pd.DataFrame): Companies - SBI activities mapper table
+def save_results(results, section):
+    write_json(f"isic_partial_results_{section}.json", results)
 
-        Returns:
-            Dict[str, List[Document]]: List of companies with top_k ISIC codes
-        """
 
-        # Group the data by ISIC sections
-        data_isic_groups = self.__group_by_isic_section(
-            companies, sbi_activities, companies_sbi_activities
-        )
+def retrieve(
+    query_documents: List[Document], doc_store, top_k
+) -> Dict[str, List[Document]]:
 
-        results = {}
-        docs = []
-        for isic_section in data_isic_groups:
+    # Group the data by ISIC sections
+    data_isic_groups = group_by_isic_section(query_documents)
 
-            query_docs = data_isic_groups[isic_section]
-            query_filter = self.__get_query_filters(isic_section)
+    results = {}
+    for isic_section in data_isic_groups:
 
+        if isic_section != "None" and int(isic_section) < 98:
+            continue
+
+        query_docs = data_isic_groups[isic_section]
+        print(len(query_docs))
+        all_query_embedding = [doc.embedding for doc in query_docs]
+
+        step = 1024
+        for i in tqdm(range(0, len(query_docs) + step, step)):
+            query_embedding = all_query_embedding[i : i + step]
             # Get embeddings of the query documents
-            embedded_docs = [doc for doc in self.embedder.run(query_docs)["documents"]]
-            query_embedding = [doc.embedding for doc in embedded_docs]
 
             # TODO: where to choose similarity function?
             # TODO: exception when there are no results returned
             # Get top_k results with the filter
-            top_results = self.doc_store.search_embeddings(
-                query_embedding, top_k=self.top_k, filters=query_filter
-            )
 
+            # TODO: every europages one results with NA
+            if isic_section == "None":
+                top_results = doc_store.search_embeddings(query_embedding, top_k=top_k)
+            else:
+                query_filter = get_query_filters(isic_section)
+                top_results = doc_store.search_embeddings(
+                    query_embedding, top_k=top_k, filters=query_filter
+                )
+            if len(top_results) == 0:
+                continue
             # Structure the retrieval results neatly
-            structured_results = self.__structure_results(query_docs, top_results)
+            structured_results = structure_results(
+                query_docs[i : i + step], top_results
+            )
             results.update(structured_results)
-            docs.extend(embedded_docs)
 
-            # Sleep to avoid reaching rate limit
-            time.sleep(3)
+    return results
 
-        return docs, results
 
-    def pretty_output(self, results: List[List[Document]]) -> Dict[str, List[List]]:
+def run(query_documents, provider, res_dir, doc_store_dir, top_k):
+    doc_store = initialise(provider, res_dir, doc_store_dir)
+    isic_results = retrieve(query_documents, doc_store, top_k)
 
-        # Get the preds and scores into a dictionary
-        output = {
-            "preds": [
-                [isic.id for isic in results[company_id]] for company_id in results
-            ],
-            "scores": [
-                [isic.score for isic in results[company_id]] for company_id in results
-            ],
-        }
-
-        return output
+    return isic_results
